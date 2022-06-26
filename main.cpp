8d4e6af362978dece9b3c71309347e07564ebebe
rhuan <rhnbnt@gmail.com>
2022-06-26 13:04:25 -0300

    lendo um labirinto por um arquivo .txt

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(){
     ifstream arquivo;
     string leitura;
-    
+
     vector<string> lab;
 
     arquivo.open("labirinto.txt");
@@ -15,13 +15,14 @@ int main(){
         for(int i = 0; i < leitura.size(); i++){
             if(leitura[i] == '#')
                 lab.push_back("-1");
-            else if(leitura[i] == '*')
+            else if(leitura[i] == '.')
                 lab.push_back("0");
         }
     }
 
     for(int t = 0; t < lab.size(); t++){
-        cout << lab[t] << endl;
+        cout << lab[t] << " ";
+        cout << endl;
     }
 
     return 0;
