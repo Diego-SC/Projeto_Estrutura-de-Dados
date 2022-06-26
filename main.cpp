52af443e55d695f6650a5e9388dcd10c906e769a
rhuan <rhnbnt@gmail.com>
2022-06-26 12:45:54 -0300

    second commmit

--- main.cpp
+++ main.cpp
@@ -1,32 +1,34 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main(){
-
     int N;
-
     cin >> N;
-    
-    char mat[N][N] = {'#','*'};
 
-    for(int i = 0; i < N;i++){
-        for(int j = 0; j < N;j++){
+    string mat[N][N];
+
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
             cin >> mat[i][j];
         }
     }
-    for(int i = 0; i < N;i++){
-        for(int j = 0; j < N;j++){
-            if(mat[i][j] == '#'){
-                mat[i][j] = -1;
+    for (int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            if(mat[i][j] == "#"){
+                mat[i][j] = "-1";
             }
-            else if(mat[i][j] == '*'){
-                mat[i][j] == 0;
+            else if(mat[i][j] == "*"){
+                mat[i][j] = "0";
             }
         }
     }
 
-    
-
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            cout << mat[i][j] << " ";
+        }
+         cout << endl;
+    }
     
 }
